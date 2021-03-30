26d9c729d135c3638fc28d7c26da4d81caa9dbe1
sp simon <cmoxsh@gmail.com>
2021-03-30 08:00:58 +0600

    project 1/2 compete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import Food from './Components/Food/Food';
+import { Container } from 'react-bootstrap';
 
 function App() {
+  // For the handle click
+  const [search, setSearch] = useState('');
+  const handleChange = event => {
+    // console.log(event.target.value);
+    setSearch(event.target.value);
+  }
+
+  // For the search vale
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+    fetch(api)
+      .then(res => res.json())
+      .then(data => setMeals(data.meals))
+  }, [search])
+  // console.log(meals.length);
+
   return (
+    // Note: This file usaged the bootstrap styles
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-container">
+        <div className="container-fluid search">
+          <h2 className="text-center">Search here for your favorite food bundle</h2>
+          <input onChange={handleChange} className="form-control bg-transparent" type="text" placeholder="Search foods"/>
+        </div>
+        <div className="text-center mb-5">
+          <h3>Your search keyword: {search}</h3>
+          <h4>Search result: {meals?.length || 0}</h4>
+        </div>
+      </div>
+
+      <Container className="food-container">
+        {
+          meals?.map(meal => <Food foods={meal} key={meal.idMeal} />)
+        }
+      </Container>
     </div>
   );
 }
---
+++ src/Components/Food/Food.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Food.css';
+// Contents Design M-ui
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
+
+const Food = (props) => {
+    const foods = props.foods;
+    const { strMealThumb, strMeal, strArea, strCategory, strTags, strInstructions, strYoutube } = foods;
+    const classes = useStyles(); // For the M-ui card
+
+    return (
+        <div className="foods-container">
+            <Card className={classes.root}>
+                <a href={strYoutube} style={{textDecoration: 'none', color: 'gray'}}>
+                    <CardActionArea>
+                        <CardMedia
+                            className={classes.media}
+                            image={strMealThumb}
+                            title="Meal-title"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">{strMeal}</Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </a>
+                <CardActions>
+                    <Button size="small" color="primary">Share</Button>
+                    <Button size="small" color="primary">Learn More</Button>
+                </CardActions>
+            </Card>
+        </div>
+    );
+};
+
+export default Food;
